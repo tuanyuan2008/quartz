12eb801a547bf791243ad9a55abb29cc1445a992
tuanyuan2008 <schen1337@gmail.com>
2025-06-02 19:42:03 -0400

    Update blog title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "move 37",
+    pageTitle: "Move 37",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
